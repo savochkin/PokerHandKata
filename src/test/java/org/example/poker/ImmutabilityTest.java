0aa3ec2981c13fbe30b197ffbc2ea7b99914bff7
Egor Savochkin <egor.savochkin@booking.com>
2025-10-22 17:49:20 +0200

    task 5.

--- src/test/java/org/example/poker/ImmutabilityTest.java
+++ src/test/java/org/example/poker/ImmutabilityTest.java
@@ -8,16 +8,38 @@ import static org.assertj.core.api.Assertions.*;
 @DisplayName("Task 5: Immutability Tests")
 class ImmutabilityTest {
 
-    // TODO: Task 5 - Implement immutability tests
-    //
-    // Acceptance Criteria:
-    // - Given a Hand, when attempting to modify the cards list via getCards().clear(), then throw UnsupportedOperationException
-    // - Given a Hand, when attempting to add a card via getCards().add(), then throw UnsupportedOperationException
-    // - Given a Hand, when attempting to remove a card via getCards().remove(), then throw UnsupportedOperationException
-    //
-    // Hint: Use assertThatThrownBy() to verify exceptions are thrown
-    // Hint: After writing the tests, fix the Hand.getCards() method to return an unmodifiable list
-    //
-    // Write your tests here:
+    @Test
+    @DisplayName("Given a Hand, when attempting to clear cards list, then throws UnsupportedOperationException")
+    void givenHand_whenAttemptingToClearCardsList_thenThrowsUnsupportedOperationException() {
+        // Given
+        Hand hand = Hand.parse("AH KD 3C TD 9S");
+
+        // When / Then
+        assertThatThrownBy(() -> hand.getCards().clear())
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("Given a Hand, when attempting to add card to list, then throws UnsupportedOperationException")
+    void givenHand_whenAttemptingToAddCardToList_thenThrowsUnsupportedOperationException() {
+        // Given
+        Hand hand = Hand.parse("AH KD 3C TD 9S");
+        Card newCard = new Card(Rank.TWO, Suit.HEARTS);
+
+        // When / Then
+        assertThatThrownBy(() -> hand.getCards().add(newCard))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
+
+    @Test
+    @DisplayName("Given a Hand, when attempting to remove card from list, then throws UnsupportedOperationException")
+    void givenHand_whenAttemptingToRemoveCardFromList_thenThrowsUnsupportedOperationException() {
+        // Given
+        Hand hand = Hand.parse("AH KD 3C TD 9S");
+
+        // When / Then
+        assertThatThrownBy(() -> hand.getCards().remove(0))
+                .isInstanceOf(UnsupportedOperationException.class);
+    }
 
 }
