6b2c04792b01b819cbffd7a90f4b36e1e8304ba6
Tatiana Ivanova <tatiana.ivanova@booking.com>
2025-10-24 13:43:14 +0200

    lombok

--- src/main/java/org/example/poker/HandRank.java
+++ src/main/java/org/example/poker/HandRank.java
@@ -2,24 +2,12 @@ package org.example.poker;
 
 import java.util.List;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class HandRank {
     private final Category category;
     private final List<Rank> kickers;
-    
-    // TODO: Task 4 - This class has too much boilerplate code. Can you improve it?
-    // Hint: Look at other classes in the project - they use something to reduce boilerplate
-    // Remove the manual constructor and getters, add @Getter and @RequiredArgsConstructor
-    
-    public HandRank(Category category, List<Rank> kickers) {
-        this.category = category;
-        this.kickers = kickers;
-    }
-    
-    public Category getCategory() {
-        return category;
-    }
-    
-    public List<Rank> getKickers() {
-        return kickers;
-    }
 }
